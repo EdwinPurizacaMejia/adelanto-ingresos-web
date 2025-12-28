df2cad78645a61a15c36b2bc70a9396c0d54b71b
EdwinPurizacaMejia <edwin.purizaca@gmail.com>
2025-12-27 23:56:50 -0500

    Actualizar estilos y componentes - 2025-12-27 23:56:50

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://2hqwzhusml.execute-api.us-east-1.amazonaws.com/Prod/'
+  apiUrl: 'https://2hqwzhusml.execute-api.us-east-1.amazonaws.com/Prod'
 };
